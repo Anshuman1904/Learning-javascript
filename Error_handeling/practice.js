9d3ef4c9497a8b7c812188e315daafd5a5c8ef46
Anshuman Sharma <anshumansharma788@gmail.com>
2025-06-20 18:58:48 +0530

    7 practice questions done

--- Error_handeling/practice.js
+++ Error_handeling/practice.js
@@ -1,23 +1,86 @@
-// write a function to write out the vowels of a string
+// foctorial 
+/*let number = 5
+let result = 1
 
-/*let str = "Hi i am Anshuman sharma"
-const newstr = (value) => {
-    return value.replace(/[aeiouAEIOU]/g, '');
-}
-console.log(newstr(str))*/
-
-/*let num = 5
-const factorial = (value) => {
-    let result = 1
-    for (let i=1; i<=value; i++) {
+const factorial = (num) => {
+    for (let i=2; i<=num; i++)
         result *= i
-    }
-    return result 
+return result;
 }
-console.log("The Factorial of the entered number is ", factorial(num))*/
+console.log("The factorial of the number is ",factorial(number))*/
 
-let str = "Myself Anshuman"
-const reversestr = (newstr) => {
-    return newstr.split(' ').reverse().join(' ')
+//remove vowels
+/*let str = "Hello myself Anshuman sharma"
+const remove = (vowels) => {
+    return str.replace(/[aeiouAEIOU]/g, '')
 }
-console.log(reversestr(str))
+console.log("The modified string is ", remove(str))*/
+
+
+//Count occurrences of a specific character in a string
+
+/*let str = "Hello I am Anshuman Sharma"
+
+const occurrence = (value,char) => {
+    let count = 0
+    value = value.toLowerCase();
+    char = char.toLowerCase();
+
+    for (let i=0; i<value.length; i++) {
+        if (value[i] === char) {
+            count++
+        }
+    }
+    return count
+}
+console.log("The occurence is ", occurrence(str,'a'))*/
+
+//reverse the words
+/*let str = "Hello the people of ahemdabad"
+
+const reversestr = (value) => {
+    return value.split(' ').reverse().join(' ')
+}
+console.log("The reverse of the given string is ", reversestr(str))*/
+
+
+// to convert the string to title case 
+/*let str = "hello myself anshuman sharma"
+const titlecase = (value) => {
+    return value.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+}
+console.log("The title case of the string is ", titlecase(str))*/
+
+
+//check if all the elements are even 
+/*let arr = [4,8,7]
+
+const alleven = (value) => {
+    for (let i=0; i<value.length; i++) {
+        if (value[i]%2 !== 0) {
+            return false
+        }
+    }
+    return true
+}
+console.log("All the elements in the array is even ", alleven(arr))*/
+
+
+// second largest number in an array 
+let arr = [34,89,88,76]
+let largest = -Infinity
+let second = -Infinity
+
+const secondlargest = (num) => {
+    for(let i=0; i<num.length; i++) {
+        let value = num[i]
+        if (value>largest) {
+            second = largest 
+            largest = value
+        }else if (value>second && value<largest) {
+            second = value
+        }
+    }
+    return second 
+}
+console.log("The second largest number of the array is ", secondlargest(arr))
