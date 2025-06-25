1c6f1ef35bdc508ea60c69d4e78e636d9e5a1b9f
Anshuman Sharma <anshumansharma788@gmail.com>
2025-06-25 16:03:31 +0530

    DOM manipulation work

---
+++ js_DOM_manipulation/script.js
@@ -0,0 +1,17 @@
+document.getElementById("heading")
+document.getElementsByClassName("text matter")
+document.getElementsByTagName
+// these are some of the ways of DOM by which we can fetch the particular in which we want to make changes in our html file
+// there are more of these that you can find on the net 
+// now these three can also be represented by (# for id), (. for class), (p for tag) you can use these instead of the full 
+// for these you will type 
+document.querySelector('#fpara')
+document.querySelector('.text matter')
+document.querySelector('p')
+// this will give only one if you have multiple in your code if same class namme same id and same tag
+// for all the elemets instead of queryselector you will type queryselectorAll 
+
+// the above are to get or fetch the elements in the html dom but for set or to edit we use 
+innerHTML
+outerHTML
+// imp this all can work in vs code just have to link it with the html file. done it in the js dom styling file refer that 
