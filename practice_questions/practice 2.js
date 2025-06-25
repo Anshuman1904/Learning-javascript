26124cba2061fc34fa77962a11215f5036cef9e7
Anshuman Sharma <anshumansharma788@gmail.com>
2025-06-25 16:11:02 +0530

    practice questions 2

---
+++ practice_questions/practice 2.js
@@ -0,0 +1,14 @@
+let studentarray = [{name: "Anshuman", marks: 87}, {name: "Ravi", marks: 75}, {name: "Sita", marks: 97}]
+
+const gradestudents = (students) => {
+      const result = []
+
+      for (let i=0; i<students.length; i++) {
+        let grade = ""
+        const marks = students[i].marks
+
+        if (marks >= 90) grade = "A"
+        else if (marks >= 70)
+          
+      }
+}
