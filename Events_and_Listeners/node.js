af73dcdccda93ea333642908df30c136a39d1d91
Anshuman Sharma <anshumansharma788@gmail.com>
2025-06-26 13:19:35 +0530

    Updated HTML and JS files

--- Events_and_Listeners/node.js
+++ Events_and_Listeners/node.js
@@ -11,11 +11,33 @@ let fpara = document.getElementById('fpara');
 fpara.addEventListener('click', changetext);*/
 // to remove it you can type fpara.removeEventListener('click', changetext);
 
-let anchorElement = document.getElementById('fanchor')
+    // let anchorElement = document.getElementById('fanchor')
 
-function handclick(event) {
-    event.preventDefault()
-    anchorElement.textContent = "click done hai"
+    // function handclick(event) {
+    //     event.preventDefault()
+    //     anchorElement.textContent = "click done hai"
+    // }
+
+    // anchorElement.addEventListener('click', handclick)
+
+//   let paras = document.querySelectorAll('p');
+  
+//   function alertpara(event) {
+//     alert("you have clicked on para: " + event.target.textContent);
+//   }
+
+//   for(let i=0; i<paras.length; i++) {
+//     let para = paras[i];
+//     para.addEventListener('click', alertpara);
+//   }
+
+let mydiv = document.getElementById('wrapper')
+
+const alertdiv = (event) => {
+    if(event.target.nodeName === 'SPAN') {
+        alert("you have clicked on the: " +  event.target.textContent) 
+    }
 }
-
-anchorElement.addEventListener('click', handclick)
+document.addEventListener('click', alertdiv)
+// target is for the event all tasks like whatever we click on in the event but nodeName is for the specifics
+// domcontentload (learn and understand from videos later )
